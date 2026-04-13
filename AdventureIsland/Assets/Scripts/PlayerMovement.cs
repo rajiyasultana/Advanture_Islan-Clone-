66c05437a2984bdab9bf7e0267e6a0f6f4cf58f3
Rajiya Sultana <rssumu04@gmail.com>
2026-04-13 16:24:36 +0600

    implement camera follow, egg logic

---
+++ AdventureIsland/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private int hitCounter = 0;
+    private bool canThrow = false;
+
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+
+    [Header("Jump")]
+    public float jumpForce = 7f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private Rigidbody rb;
+    private float moveInput;
+    private bool isGrounded;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Check ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    void FixedUpdate()
+    {
+        // Move left/right
+        Vector3 velocity = rb.linearVelocity;
+        velocity.x = moveInput * moveSpeed;
+        rb.linearVelocity = velocity;
+    }
+
+    
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+        moveInput = input.x;
+    }
+
+    
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+        }
+    }
+
+    public void OnThrow(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            // Implement throw logic here
+            Debug.Log("Throw action performed");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Egg"))
+        {
+            hitCounter++;
+            if(hitCounter >= 2)
+            {
+                canThrow = true;
+                Debug.Log("Player can now throw!");
+                Destroy(collision.gameObject); // Destroy the egg after hitting it twice
+            }
+        }
+    }
+}
